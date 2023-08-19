eda05432a60aa3a60a51af303577a0ad96a9b981
thednp <thednp@users.noreply.github.com>
2023-08-19 22:40:29 +0300

    format

--- example/index.tsx
+++ example/index.tsx
@@ -1,4 +1,4 @@
-import { render } from 'solid-js/web'
-import App from './App'
+import { render } from 'solid-js/web';
+import App from './App';
 
 render(() => <App />, document.getElementById('root')!);
