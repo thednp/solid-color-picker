68d1bcfab0cda6ff877534687d3691284df0ecc8
thednp <thednp@users.noreply.github.com>
2023-08-27 12:40:22 +0300

    Delete src directory

--- src/index.tsx
+++
@@ -1,3 +0,0 @@
-import DefaultColorPicker from './components/DefaultColorPicker';
-
-export { DefaultColorPicker };
--- src/util/initialControlPositions.ts
+++
@@ -1,8 +0,0 @@
-const initialControlPositions = {
-  c1x: 0,
-  c1y: 0,
-  c2y: 0,
-  c3y: 0,
-};
-
-export default initialControlPositions;
