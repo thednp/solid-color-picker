5e7d901f5c539424ff066732245fdbf6730dc0bd
thednp <thednp@users.noreply.github.com>
2023-08-27 12:59:34 +0300

    fix deploy

---
+++ example/vite.config.docs.ts
@@ -0,0 +1,34 @@
+import { defineConfig } from 'vite';
+import solidPlugin from 'vite-plugin-solid';
+import solidSVG from 'vite-plugin-solid-svg';
+
+export default defineConfig({
+  plugins: [
+    solidPlugin(),
+    solidSVG({ defaultAsComponent: true }),
+    {
+      name: 'Reaplace env variables',
+      transform(code, id) {
+        if (id.includes('node_modules')) {
+          return code;
+        }
+        return code
+          .replace(/process\.env\.SSR/g, 'false')
+          .replace(/process\.env\.DEV/g, 'true')
+          .replace(/process\.env\.PROD/g, 'false')
+          .replace(/process\.env\.NODE_ENV/g, '"development"')
+          .replace(/import\.meta\.env\.SSR/g, 'false')
+          .replace(/import\.meta\.env\.DEV/g, 'true')
+          .replace(/import\.meta\.env\.PROD/g, 'false')
+          .replace(/import\.meta\.env\.NODE_ENV/g, '"development"');
+      },
+    },
+  ],
+  server: {
+    port: 3000,
+  },
+  build: {
+    target: 'esnext',
+    outDir: '../docs',
+  },
+});
